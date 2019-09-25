3455e4df4cf8c2c012b5b292a36040356933160c
lyno4ka <yuliyamasterenko@gmail.com>
2019-09-25 15:12:29 +0300

    Create css and js

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ function getQuantityElements(heightElement){
 
 function startGame(){
 start.classList.add('hide');
+gameArea.innerHTML = '';
 
 for (let i = 0; i < getQuantityElements(100); i++) {
 const line = document.createElement('div');
@@ -45,11 +46,14 @@ enemy.style.left = Math.floor((Math.random() * (gameArea.offsetWidth - 50))) + '
 enemy.style.top = enemy.y + 'px';
 enemy.style.background = 'transparent url("./enemy2.png") center / cover no-repeat';
 gameArea.appendChild(enemy);
-
 }
 
+setting.score = 0;
 setting.start = true;
 gameArea.appendChild(car);
+car.style.left = gameArea.offsetWidth/2 - car.offsetWidth/2;
+car.style.top = 'auto';
+car.style.bottom = '10px';
 setting.x = car.offsetLeft;
 setting.y = car.offsetTop;
 requestAnimationFrame(playGame);
@@ -58,6 +62,8 @@ requestAnimationFrame(playGame);
 function playGame(){
 
 if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = 'SCORE<br>' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -98,15 +104,29 @@ lines.forEach(function(line){
 line.y += setting.speed;
 line.style.top = line.y + 'px';
 
-if (line.y >= document.documentElement.clientHeight)
-line.y = -100;
+    if (line.y >= document.documentElement.clientHeight){
+        line.y = -100;
+    } 
+ 
 });
 }
 
 function moveEnemy() {
-    let enemy = document.querySelectorAll('.enemy')
+    let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function (item){
-    item. y += setting.speed / 2;
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom &&
+        carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right &&
+        carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight;
+    }
+
+    item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
     if (item.y >= document.documentElement.clientHeight) {
         item.y = -100 * setting.traffic;
